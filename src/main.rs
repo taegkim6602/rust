45be7807c2ca7221d8fe393c9c0ddfa87c07fe4d
taegyoon <ultra.s.m.tae1@gmail.com>
2024-05-29 07:31:36 +0900

    before conntecting with postgres DB

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
-use zero2Prod::run;
+use zero2Prod::configuration::get_configuration;
+use zero2Prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read Configutation");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
